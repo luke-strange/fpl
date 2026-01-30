40a8e2ca76f340530a0fcf136b347a2b6b788fd0
luke-strange <92686634+luke-strange@users.noreply.github.com>
2026-01-30 21:09:52 +0000

    fix all the problems

--- pipelines/bootstrap_static.py
+++ pipelines/bootstrap_static.py
@@ -12,12 +12,20 @@ if __name__ == "__main__":
     if res.status_code == 200:
         data = res.json()
         
-        player_codes = {e["id"]: e["web_name"] for e in data["elements"]}
-        with open(ROOT / "data/lookups/player_codes.json", "w") as f:
+        player_codes = {
+            e["id"]: 
+            {"web_name": e["web_name"], "full_name": e["first_name"] + " " + e["second_name"]} 
+            for e in data["elements"]
+        }
+        with open(ROOT / "src/_data/lookups/playerCode2Name.json", "w") as f:
             json.dump(player_codes, f)
 
         element_stats = {es["name"]: es["label"] for es in data["element_stats"]}
-        with open(ROOT / "data/lookups/element_stats.json", "w") as f:
+        with open(ROOT / "src/_data/lookups/element_stats.json", "w") as f:
+            json.dump(element_stats, f)
+        
+        teams = {t["id"]: {"name": t["name"], "short_name": ["short_name"]} for t in data["teams"]}
+        with open(ROOT / "src/_data/lookups/element_stats.json", "w") as f:
             json.dump(element_stats, f)
     else:
         print(res.status_code)
